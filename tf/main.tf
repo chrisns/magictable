3cdac49e25bc3a829dfdeca2f1cdb970ab559a0c
Chris Nesbitt-Smith <chris@cns.me.uk>
2020-06-01 19:08:04 +0100

    add more domains

--- tf/main.tf
+++ tf/main.tf
@@ -26,9 +26,64 @@ output "demo" {
 
 module "rotten100films" {
   source     = "./modules/bucket"
-  url       = "www.rottenfilms100.com"
+  url       = "www.rotten100films.com"
 }
 
 output "rotten100films" {
     value = module.rotten100films.dns
 }
+
+module "rottenfilms100" {
+  source     = "./modules/bucket"
+  url       = "www.rottenfilms100.com"
+}
+
+output "rottenfilms100" {
+    value = module.rottenfilms100.dns
+}
+
+module "thetoptomato100" {
+  source     = "./modules/bucket"
+  url       = "www.thetoptomato100.com"
+}
+
+output "thetoptomato100" {
+    value = module.thetoptomato100.dns
+}
+
+module "rt100films" {
+  source     = "./modules/bucket"
+  url       = "www.rt100films.com"
+}
+
+output "rt100films" {
+    value = module.rt100films.dns
+}
+
+module "tomatoes100" {
+  source     = "./modules/bucket"
+  url       = "www.tomatoes100.com"
+}
+
+output "tomatoes100" {
+    value = module.tomatoes100.dns
+}
+
+module "toms100films" {
+  source     = "./modules/bucket"
+  url       = "www.toms100films.com"
+}
+
+output "toms100films" {
+    value = module.toms100films.dns
+}
+
+module "rt100movies" {
+  source     = "./modules/bucket"
+  url       = "www.rt100movies.com"
+}
+
+output "rt100movies" {
+    value = module.rt100movies.dns
+}
+
