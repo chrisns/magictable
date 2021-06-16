5f295ddcfadd169ea0f71c1c7fc3588f498cd38b
Chris Nesbitt-Smith <chris@cns.me.uk>
2021-06-16 11:38:53 +0100

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -36,14 +36,9 @@ resource "aws_iam_policy" "policy" {
               "arn:aws:s3:::www.rotten100.com/index.html",
               "arn:aws:s3:::www.rotten100.net/index.html",
               "arn:aws:s3:::www.rottenfilms100.com/index.html",
-              "arn:aws:s3:::www.rotten100films.com/index.html",
-              "arn:aws:s3:::www.rt100films.com/index.html",
-              "arn:aws:s3:::www.rt100movies.com/index.html",
               "arn:aws:s3:::www.thetoptomato100.com/index.html",
-              "arn:aws:s3:::www.tomatoes100.com/index.html",
               "arn:aws:s3:::www.tomatoes100.net/index.html",
-              "arn:aws:s3:::www.rotten100.uk",
-              "arn:aws:s3:::www.toms100films.com/index.html"
+              "arn:aws:s3:::www.rotten100.uk"
             ]
         }
     ]
@@ -66,11 +61,6 @@ module "demo" {
   url       = "demo.zoomfab.info"
 }
 
-module "rotten100films" {
-  source     = "./modules/bucket"
-  url       = "www.rotten100films.com"
-}
-
 module "rottenfilms100" {
   source     = "./modules/bucket"
   url       = "www.rottenfilms100.com"
@@ -79,26 +69,6 @@ module "rottenfilms100" {
 module "thetoptomato100" {
   source     = "./modules/bucket"
   url       = "www.thetoptomato100.com"
-}
-
-module "rt100films" {
-  source     = "./modules/bucket"
-  url       = "www.rt100films.com"
-}
-
-module "tomatoes100" {
-  source     = "./modules/bucket"
-  url       = "www.tomatoes100.com"
-}
-
-module "toms100films" {
-  source     = "./modules/bucket"
-  url       = "www.toms100films.com"
-}
-
-module "rt100movies" {
-  source     = "./modules/bucket"
-  url       = "www.rt100movies.com"
 }
 
 module "_100rotten_com" {
